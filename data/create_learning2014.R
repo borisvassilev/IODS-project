820a0a634931d20885bd725fc3d84879a3386227
Boris Vassilev <boris.vassilev@gmail.com>
2018-11-12 05:51:31 +0200

    Combining variables

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -14,9 +14,6 @@ str(lrn14)
 #     the last one is a factor with 2 levels,
 #     F and M (female and male)
 
-# divide each number in a vector
-c(1,2,3,4,5) / 2
-
 # print the "Attitude" column vector of the lrn14 data
 lrn14$Attitude
 
@@ -25,4 +22,25 @@ lrn14$Attitude / 10
 
 # create column 'attitude' by scaling the column "Attitude"
 lrn14$attitude <- lrn14$Attitude / 10
+
+# Access the dplyr library
+library(dplyr)
+
+# questions related to deep, surface and strategic learning
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+# select the columns related to deep learning and create column 'deep' by averaging
+deep_columns <- select(lrn14, one_of(deep_questions))
+lrn14$deep <- rowMeans(deep_columns)
+
+# select the columns related to surface learning and create column 'surf' by averaging
+surface_columns <- select(lrn14, one_of(surface_questions))
+lrn14$surf <- rowMeans(surface_columns)
+
+# select the columns related to strategic learning and create column 'stra' by averaging
+strategic_columns <- select(lrn14, one_of(strategic_questions))
+lrn14$stra <- rowMeans(strategic_columns)
+
 
